4f4f3111ee2d89fd135df2595e866b03477b9175
Manjeet Chauhan <manjeetboy00@gmail.com>
2026-02-24 06:43:01 +0530

    Updated Lenis package

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+// import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function SmoothScroll() {
   useEffect(() => {
